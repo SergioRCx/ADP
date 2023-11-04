3905d6d7dd82c740650ddee5695913ca9bdbd1bb
SergioRCx <143467133+SergioRCx@users.noreply.github.com>
2023-11-03 19:04:11 -0600

    Update holaMundo.py

--- holaMundo.py
+++ holaMundo.py
@@ -1,6 +1,7 @@
 import streamlit as st
 def main():
-    st.title('¡Hola Mundooo!')
+    st.subheader("Bienvenido a mi sitio")
+    st.title("¡Hola Mundo!")
     st.write('Sergio RC')
 if __name__ == '__main__':
     main()
