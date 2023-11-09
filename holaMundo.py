5f9a9027e15cbd106a3d0ea9921f0351eec1c190
SergioRCx <143467133+SergioRCx@users.noreply.github.com>
2023-11-08 20:14:50 -0600

    Update holaMundo.py

--- holaMundo.py
+++ holaMundo.py
@@ -1,7 +1,24 @@
 import streamlit as st
+
 def main():
-   st.subheader("Bienvenido a mi sitio") 
-   st.title("¡Hola Mundo!") 
-   st.write('Sergio RC')
+    st.title('¡Hola Mundo!')
+    st.markdown(
+        """
+        <style>
+        body {
+            text-align: center;
+            color: aqua;
+            font-size: 50px;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    st.write('Sergio Alexis Romero Carreto')
+    st.write('NC: 19091427')
+#text-align: center;
+#color: aqua;
+#font-size: 50px;
 if __name__ == '__main__':
     main()
+
