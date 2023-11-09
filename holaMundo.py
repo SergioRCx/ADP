93cf57fae5d824a1e9843e1e2fa37c3ac1918f4c
SergioRCx <143467133+SergioRCx@users.noreply.github.com>
2023-11-08 20:18:16 -0600

    Update holaMundo.py

--- holaMundo.py
+++ holaMundo.py
@@ -3,7 +3,7 @@ import streamlit as st
 def main():
     st.title('¡Hola Mundo!')
     st.write('<p style="text-align: center; color: aqua;">Sergio Alexis Romero Carreto</p>', unsafe_allow_html=True)
-    st.write('NC: 19091427')
+    st.write('<p style="text-align: center; color: aqua;">NC: 19091427</p>', unsafe_allow_html=True')
 #text-align: center;
 #color: aqua;
 #font-size: 50px;
