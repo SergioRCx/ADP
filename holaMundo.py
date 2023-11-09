6ea98e028fc2741eaaa86d827b34af25ea048ed9
SergioRCx <143467133+SergioRCx@users.noreply.github.com>
2023-11-08 20:27:45 -0600

    Update holaMundo.py

--- holaMundo.py
+++ holaMundo.py
@@ -13,7 +13,7 @@ def main():
     )
     st.title('¡Hola Mundo!')
     st.write('<p style="font-size: 50px;color: aqua;">Sergio Alexis Romero Carreto</p>', unsafe_allow_html=True)
-    st.write('<p style="font-size: 50px;color: aqua;">NC: 19091427</p>', unsafe_allow_html=True)
+    st.write('<p style="font-size: 40px;color: aqua;">NC: 19091427</p>', unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()
